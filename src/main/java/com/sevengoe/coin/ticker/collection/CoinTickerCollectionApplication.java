e76a7d70ee79b4576feb19905e2f548db1503361
김병준 <sevengoe@naver.com>
2022-04-10 01:41:01 +0900

    init commit

---
+++ src/main/java/com/sevengoe/coin/ticker/collection/CoinTickerCollectionApplication.java
@@ -0,0 +1,13 @@
+package com.sevengoe.coin.ticker.collection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinTickerCollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinTickerCollectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sevengoe/coin/ticker/collection/CoinTickerCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sevengoe.coin.ticker.collection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinTickerCollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
